e2e08e41b28a442378982c50972155f37053b909
Carlos Diaz <carlos.cedp@gmail.com>
2022-02-10 10:10:51 -0400

    page 700

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -4,20 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.Optimization;
 
-namespace ClientFeatures.App_Start
+namespace ClientFeatures
 {
     public class BundleConfig
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
          
-            bundles.Add( new StyleBundle("∼/Content/css")
+            bundles.Add( new StyleBundle("~/Content/css")
                 .Include(
                 "~/Content/*.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/clientfeaturesscripts")
                 .Include(
-                "∼/Scripts/jquery-{version}.js",
+                "~/Scripts/jquery-{version}.js",
                 "~/Scripts/jquery.validate.js",
                 "~/Scripts/jquery.validate.unobtrusive.js",
                 "~/Scripts/jquery.unobtrusive-ajax.js"));
